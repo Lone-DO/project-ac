51aa328301770ae6911a026541d841919dede1d2
lone-DO <citizin@outlook.com>
2025-07-09 00:32:51 +0200

    Migrate React Configurations to VITE

---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig, type UserConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import config from './src/assets/common/config';
+
+// https://vite.dev/config/
+export default defineConfig(async ({ mode }): Promise<UserConfig> => {
+	await config.init(mode);
+
+	return {
+		base: config.isPlugin
+			? '/project-animalcrossing/dist/'
+			: config.isProduction
+				? '/project-animalcrossing/'
+				: './',
+		esbuild: {
+			// Remove debugger statements in production
+			drop: mode === 'production' ? ['debugger'] : [],
+		},
+		server: {
+			port: 8082,
+		},
+		plugins: [react()],
+		build: {
+			emptyOutDir: true,
+			minify: false,
+			sourcemap: true,
+			rollupOptions: {
+				output: {
+					name: 'react-portfolio',
+					entryFileNames: (chunk) => `${[chunk.name]}.js`,
+				},
+			},
+		},
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url)),
+			},
+		},
+		css: {
+			devSourcemap: true,
+			preprocessorOptions: {
+				scss: {
+					sourceMapIncludeSources: true,
+				},
+			},
+		},
+	};
+});
