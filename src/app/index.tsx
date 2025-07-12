5154454cbc16334a4a4cb1bc53033ad4b1dfa1fe
lone-DO <citizin@outlook.com>
2025-07-12 02:50:18 +0200

    App - Initialize Timer TickTock

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import '@/assets/styles/vendor/materialize/materialize.scss';
 import './index.scss';
@@ -10,20 +10,37 @@ import Footer from '@/components/_footer';
 // import audioData from '@/common/audioData';
 
 import loadingGif from '@/assets/images/loading.gif';
+import { useDispatch } from 'react-redux';
+import { sync } from '@/features/timer/timerApi';
 
 function App() {
+	const isClosed = false;
+	const dispatch = useDispatch();
 	const [isLoaded, setLoaded] = useState(false);
-	function init() {
-		/** TODO: MLabs was taken down years ago and merged into MongoDB, must rebuild database */
-		// 	fetch(
-		// 		'https://api.mlab.com/api/1/databases/lone-do/collections/albums?apiKey=9P6rUGDfq5OxFXag9RZYNkk3U2vF6IT0',
-		// 	)
-		// 		.then((res) => res.json())
-		// 		.then((res) => this.setState({ videoData: res, isLoaded: true }));
-		setLoaded(true);
-	}
 
-	useEffect(() => init(), [isLoaded]);
+	const debounce = useRef<NodeJS.Timeout | string | number>('');
+	/** ClearTimeout on component closure */
+	if (isClosed) {
+		clearTimeout(debounce.current);
+	}
+	const startCycle = useCallback(() => {
+		debounce.current = setTimeout(() => {
+			dispatch(sync());
+			return startCycle();
+		}, 3000);
+	}, [dispatch]);
+	useEffect(() => {
+		if (!isLoaded) {
+			/** TODO: MLabs was taken down years ago and merged into MongoDB, must rebuild database */
+			// 	fetch(
+			// 		'https://api.mlab.com/api/1/databases/lone-do/collections/albums?apiKey=9P6rUGDfq5OxFXag9RZYNkk3U2vF6IT0',
+			// 	)
+			// 		.then((res) => res.json())
+			// 		.then((res) => this.setState({ videoData: res, isLoaded: true }));
+			setLoaded(true);
+			startCycle();
+		}
+	}, [dispatch, isLoaded, startCycle]);
 	return (
 		<div className='App'>
 			<div className='App-body ACTunes_body'>
